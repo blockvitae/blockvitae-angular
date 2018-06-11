3c1b9f1b8dc56cfe2caf092a0102c36543f24d0f
sidharth <sidharthmalhotra0094@gmail.com>
2018-06-11 14:24:55 -0400

    [TASK] Initialized Truffle

---
+++ ethereum/migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+/**
+ * This file deploys all the compiled smart contracts
+ */
+
+ module.exports = function(deployer) {
+     // deploy contracts
+ }
---
+++ ethereum/truffle.js
@@ -0,0 +1,19 @@
+module.exports = {
+  solc: {
+    optimizer: {
+      // disabled by default
+      enabled: true,
+      // Optimize for how many times you intend to run the code.
+      // Lower values will optimize more for initial deployment cost, 
+      // higher values will optimize more for high-frequency usage.
+      runs: 500
+    },
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
