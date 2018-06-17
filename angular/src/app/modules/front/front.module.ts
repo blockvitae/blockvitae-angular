5a11d5f951f4ad9c4dc43ffcf19d3fa7b9484c5e
sidharth <sidharthmalhotra0094@gmail.com>
2018-06-17 15:55:04 -0400

    [WIP] Signup component initialized

--- angular/src/app/modules/front/front.module.ts
+++ angular/src/app/modules/front/front.module.ts
@@ -2,15 +2,19 @@ import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, SignupComponent],
   exports: [
     LandingComponent,
+    SignupComponent,
   ]
 })
 export class FrontModule { }
--- angular/src/app/routes/blockvitae-routes.ts
+++ angular/src/app/routes/blockvitae-routes.ts
@@ -1,6 +1,8 @@
+import { SignupComponent } from './../modules/front/signup/signup.component';
 import { LandingComponent } from './../modules/front/landing/landing.component';
 import { Routes } from '@angular/router';
 
 export const blockvitaeRoutes: Routes = [
+  {path: 'signup', component: SignupComponent},
   {path: '', component: LandingComponent},
 ];
