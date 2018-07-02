62e06e19672722569cdb5ce460d484e8ab849943
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-01 23:48:16 -0400

    [WIP] Added skills input to tag creater in skills dialog

--- src/app/modules/dashboard/dialog/skills-dialog/skills-dialog.component.ts
+++ src/app/modules/dashboard/dialog/skills-dialog/skills-dialog.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-skills-dialog',
   templateUrl: './skills-dialog.component.html',
   styleUrls: ['./skills-dialog.component.scss']
 })
-export class SkillsDialogComponent implements OnInit {
+export class SkillsDialogComponent {
 
-  constructor() { }
+  // binded with input
+  // holds string of skills
+  public skills: string;
 
-  ngOnInit() {
+  // string array to hold skills
+  public userSkills: string[];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public userSkill: any
+  ) { 
+    this.skills = "";
+    this.userSkills = [];
   }
 
+  public onSpaceSkills(event): void {
+    // 188 is keycode for comma
+    if (event.keyCode === 188) {
+      this.userSkills = this.skills.split(",");
+    }
+  }
 }
