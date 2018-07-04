1564b8a128212b08fc816dd0deae583aea724e38
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-04 12:49:22 -0400

    [WIP] Added binding for userProject to project dialog

--- src/app/modules/dashboard/dialog/projects-dialog/projects-dialog.component.ts
+++ src/app/modules/dashboard/dialog/projects-dialog/projects-dialog.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-projects-dialog',
   templateUrl: './projects-dialog.component.html',
   styleUrls: ['./projects-dialog.component.scss']
 })
-export class ProjectsDialogComponent implements OnInit {
+export class ProjectsDialogComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public userProject: Blockvitae.userProject
+  ) { }
 }
