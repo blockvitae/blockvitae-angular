5fdd91c996bd7099d0dd6fa40fd8bee57b8579d8
sidharth <sidharthmalhotra0094@gmail.com>
2018-07-07 23:52:00 -0400

    [TASK] Added show public view button only for profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CheckMetamaskService } from './services/check-metamask.service';
+import { LocationStrategy } from '../../node_modules/@angular/common';
 
 const BTN_TEXT_VIEW_PUBLIC = "View Public Profile";
 const BTN_TEXT_HIDE_PUBLIC = "Edit Profile";
@@ -15,12 +16,21 @@ export class AppComponent {
 
   public viewPublic: boolean;
 
+  public showBtn: boolean;
+
   constructor(
-    private checkMetamask: CheckMetamaskService
+    private checkMetamask: CheckMetamaskService,
+    private url: LocationStrategy
   ) {
     this.checkMetamask.initializeDappBrowser();
     this.btnText = BTN_TEXT_VIEW_PUBLIC;
     this.viewPublic = false;
+
+    // show button only if on profile page
+    if (this.url.path().indexOf('/resume') < 0)
+      this.showBtn = false;
+    else
+      this.showBtn = true;
   }
 
   /**
